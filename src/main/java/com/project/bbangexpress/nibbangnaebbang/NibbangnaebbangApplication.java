cd18aa0d8a0ab6969445a6f73ccebb2d825e42ba
JuYeon-Jin <juuju.00b@gmail.com>
2023-10-14 14:17:02 +0900

    [ADD] Spring Initializr Project Import
    Spring Initializr 기본설정

---
+++ src/main/java/com/project/bbangexpress/nibbangnaebbang/NibbangnaebbangApplication.java
@@ -0,0 +1,13 @@
+package com.project.bbangexpress.nibbangnaebbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NibbangnaebbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NibbangnaebbangApplication.class, args);
+	}
+
+}
